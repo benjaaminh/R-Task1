b70eae8e2250718c1881c6d17494154ac9e26101
benjaaminh <benjamin.heikkala@abo.fi>
2024-03-18 15:58:40 +0200

    median result

--- uppg1.R
+++ uppg1.R
@@ -1,7 +1,7 @@
 data <- read.csv("SUSsvarNy23.csv", sep=";")
 
 
-rad_index <- 4
+rad_index <- 1
 
 specifik_rad <- data[rad_index, ]
 
@@ -9,10 +9,17 @@ specifik_rad_värden <- as.numeric(specifik_rad[])
 
 medelvärde_rad <- mean(specifik_rad_värden)
 
+median_rad <- median(specifik_rad_värden)
+
 medelvärden_rader <- apply(data, 1, mean, na.rm = TRUE) #ger medelvärdet av varje individuell rad
+
+medianer_rader <- apply(data, 1, median, na.rm = TRUE)
+
+median <- median(medianer_rader) #medianvärdet
 
 medelvärde <- mean(medelvärden_rader) #ger medelvärdet av alla rader
 
-print(medelvärde)
+
+print(median)
 
 
