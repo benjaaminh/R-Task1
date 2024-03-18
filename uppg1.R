5d9187fc6d8cdc531e0aca9ffa91ca82ca6a5c96
eerikpeerik <erik.astrom@abo.fi>
2024-03-18 16:41:06 +0200

    Translated code and comments into English.

--- uppg1.R
+++ uppg1.R
@@ -1,31 +1,21 @@
 data <- read.csv("SUSsvarNy23.csv", sep=";")
 
 # gränserna för nya normaliserad data
-nytt_min <- 0
-nytt_max <- 100
+new_min <- 0
+new_max <- 100
 
 #normaliserar data från skala 1-5 till 0-100 (SUS)
-normaliserad_data <- ((data - min(data)) / (max(data) - min(data))) * (nytt_max - nytt_min) + nytt_min
+normalised_data <- ((data - min(data)) / (max(data) - min(data))) * (new_max - new_min) + new_min
 
-rad_index <- 1
+meanValue_row <- apply(normalised_data, 1, mean, na.rm = TRUE)    # calculates the mean value of every indiviual row, individually.
 
-specifik_rad <- normaliserad_data[rad_index, ]
+median_row <- apply(normalised_data, 1, median, na.rm = TRUE)     # calcualtes median for every indiviual row, individually. 
 
-specifik_rad_värden <- as.numeric(specifik_rad[])
+median <- median(median_row)        # calcluates the median of all rows
 
-medelvärde_rad <- mean(specifik_rad_värden)
+meanValue <- mean(meanValue_row)   # calculates the mean value of all rows
 
-median_rad <- median(specifik_rad_värden)
-
-medelvärden_rader <- apply(normaliserad_data, 1, mean, na.rm = TRUE)    # ger medelvärdet av varje individuell rad
-
-medianer_rader <- apply(normaliserad_data, 1, median, na.rm = TRUE)     # median för alla individuella rader
-
-median <- median(medianer_rader)        # medianvärdet
-
-medelvärde <- mean(medelvärden_rader)   # ger medelvärdet av alla rader
-
-#print(summary(normaliserad_data))
-print(medelvärde_rad)
+#print(summary(normalised_data))
+print(meanValue_rad)
 
 
