6f6db3b4c4668569f1a6d4e2f5e296a4bf64de06
benja <benjamin.heikkala@abo.fi>
2024-03-18 18:03:41 +0200

    some refractoring of code

--- uppg1.R
+++ uppg1.R
@@ -1,31 +1,31 @@
 data <- read.csv("SUSsvarNy23.csv", sep=";")
 
-# gränserna för nya normaliserad data
+# limits for normalized data 0-100
 new_min <- 0
 new_max <- 100
 
-#normaliserar data från skala 1-5 till 0-100 (SUS)
-normalised_data <- ((data - min(data)) / (max(data) - min(data))) * (new_max - new_min) + new_min
+#normalizing data from scale 1-5 to 0-100 (SUS)
+normalized_data <- ((data - min(data)) / (max(data) - min(data))) * (new_max - new_min) + new_min
 
-meanValue_row <- apply(normalised_data, 1, mean, na.rm = TRUE)    # calculates the mean value of every indiviual row, individually.
+meanValue_row <- apply(normalized_data, 1, mean, na.rm = TRUE)    # calculates the mean value of every indiviual row
 
-medianer_rader <- apply(normaliserad_data, 1, median, na.rm = TRUE)     # median för alla individuella rader
+medianer_rader <- apply(normalized_data, 1, median, na.rm = TRUE)     # median for all individual rows
+
+minimum_each_row <- apply(normalized_data, 1, min, na.rm = TRUE) #minimum value for each row
+
+maximum_each_row <- apply(normalized_data, 1, max, na.rm = TRUE) #maximum value for each row
+
+standarddeviation_each_row <- apply(normalized_data, 1, sd, na.rm = TRUE) #standard deviation for each row
 
 median <- median(medianer_rader)        # calcluates the median of all rows
 
 meanValue <- mean(meanValue_row)   # calculates the mean value of all rows
 
-minimum_each_row <- apply(normaliserad_data, 1, min, na.rm = TRUE) #minimum value for each row
+standard_deviation <- sd(standarddeviation_each_row) #standard deviation for all rows
 
-maximum_each_row <- apply(normaliserad_data, 1, max, na.rm = TRUE) #maximum value for each row
+min <- min(minimum_each_row) #minimum value of all rows
 
-standarddeviation_each_row <- apply(normaliserad_data, 1, sd, na.rm = TRUE) #standard deviation for each row
-
-standard_deviation <- sd(standarddeviation_each_row)
-
-min <- min(minimum_each_row)
-
-max <- max(maximum_each_row)
+max <- max(maximum_each_row) #maximum value of all rows
 #print(summary(normaliserad_data))
 cat("Medelvärde av SUS-resultaten:", meanValue, "\n")
 cat("Median av SUS-resultaten:", median, "\n")
