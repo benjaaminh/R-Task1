34f4ea6a562eab72ad880d27495d2cbf17d0e780
benjaaminh <benjamin.heikkala@abo.fi>
2024-03-18 16:24:20 +0200

    normaliserad till 0-100

--- uppg1.R
+++ uppg1.R
@@ -1,9 +1,15 @@
 data <- read.csv("SUSsvarNy23.csv", sep=";")
 
+# gränserna för nya normaliserad data
+nytt_min <- 0
+nytt_max <- 100
+
+#normaliserar data från skala 1-5 till 0-100 (SUS)
+normaliserad_data <- ((data - min(data)) / (max(data) - min(data))) * (nytt_max - nytt_min) + nytt_min
 
 rad_index <- 1
 
-specifik_rad <- data[rad_index, ]
+specifik_rad <- normaliserad_data[rad_index, ]
 
 specifik_rad_värden <- as.numeric(specifik_rad[])
 
@@ -11,15 +17,15 @@ medelvärde_rad <- mean(specifik_rad_värden)
 
 median_rad <- median(specifik_rad_värden)
 
-medelvärden_rader <- apply(data, 1, mean, na.rm = TRUE) #ger medelvärdet av varje individuell rad
+medelvärden_rader <- apply(normaliserad_data, 1, mean, na.rm = TRUE) #ger medelvärdet av varje individuell rad
 
-medianer_rader <- apply(data, 1, median, na.rm = TRUE)
+medianer_rader <- apply(normaliserad_data, 1, median, na.rm = TRUE) #median för alla individuella rader
 
 median <- median(medianer_rader) #medianvärdet
 
 medelvärde <- mean(medelvärden_rader) #ger medelvärdet av alla rader
 
-
-print(median)
+#print(summary(normaliserad_data))
+print(medelvärde_rad)
 
 
