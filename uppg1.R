e16ab75719124a5d7929434fc8e70a04791f252a
benjaaminh <benjamin.heikkala@abo.fi>
2024-04-04 09:17:18 +0300

    improved some comments and variable names

--- uppg1.R
+++ uppg1.R
@@ -6,12 +6,12 @@ data <- read.csv("SUSsvarNy23.csv", sep=";")#read data and define semicolon as s
 new_min <- 0
 new_max <- 100
 
-#normalizing data from scale 1-5 to 0-100 (SUS)
+#normalizing data from scale 1-5 to 0-100
 normalized_data <- ((data - min(data)) / (max(data) - min(data))) * (new_max - new_min) + new_min
 
-meanValue_row <- apply(normalized_data, 1, mean, na.rm = TRUE)    # calculates the mean value of every indiviual row
+mean_each_row <- apply(normalized_data, 1, mean, na.rm = TRUE)    # calculates the mean value of every indiviual row
 
-medianer_rader <- apply(normalized_data, 1, median, na.rm = TRUE)     # median for all individual rows
+median_each_row <- apply(normalized_data, 1, median, na.rm = TRUE)     # median for all individual rows
 
 minimum_each_row <- apply(normalized_data, 1, min, na.rm = TRUE) #minimum value for each row
 
@@ -19,17 +19,17 @@ maximum_each_row <- apply(normalized_data, 1, max, na.rm = TRUE) #maximum value 
 
 standarddeviation_each_row <- apply(normalized_data, 1, sd, na.rm = TRUE) #standard deviation for each row
 
-median <- median(medianer_rader)        # calcluates the median of all rows
+median <- median(median_each_row)        # calcluates the median of all rows
 
-meanValue <- mean(meanValue_row)   # calculates the mean value of all rows
+mean <- mean(mean_each_row)   # calculates the mean value of all rows
 
 standard_deviation <- sd(standarddeviation_each_row) #standard deviation for all rows
 
 min <- min(minimum_each_row) #minimum value of all rows
 
 max <- max(maximum_each_row) #maximum value of all rows
-#print(summary(normaliserad_data))
-cat("Medelvärde av SUS-resultaten:", meanValue, "\n")
+#print results
+cat("Medelvärde av SUS-resultaten:", mean, "\n")
 cat("Median av SUS-resultaten:", median, "\n")
 cat("Minimum värde av SUS-resultaten:", min, "\n")
 cat("Maximum värde av SUS-resultaten:", max, "\n")
