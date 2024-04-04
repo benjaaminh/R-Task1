9f97fc91ffd5c49aeb1fa1e30c97e2e126fd2935
benjaaminh <benjamin.heikkala@abo.fi>
2024-04-04 09:58:40 +0300

    improved normalization

--- uppg1.R
+++ uppg1.R
@@ -1,33 +1,24 @@
-#TODO: check if there is a better way of normalizing data, check if there is a better way of retrieving median, mean etc..
-
 data <- read.csv("SUSsvarNy23.csv", sep=";")#read data and define semicolon as separator
 
-# limits for normalized data 0-100
-new_min <- 0
-new_max <- 100
+normalize_to_SUS <- function(row) {
+    sum_odd <- sum(row[c(TRUE, FALSE)]) - 5 #calculates sum of all odd numbered questions
+    sum_even <- 25 - sum(row[c(FALSE, TRUE)]) #calculates sum of all even numbered questions
 
-#normalizing data from scale 1-5 to 0-100
-normalized_data <- ((data - min(data)) / (max(data) - min(data))) * (new_max - new_min) + new_min
+    normalized_data <- (sum_odd + sum_even) * 2.5 #normalize all data
+    return (normalized_data)
+}
+normalized_each_row <- apply(data, 1, normalize_to_SUS) #calculates SUS score for each row in dataset
 
-mean_each_row <- apply(normalized_data, 1, mean, na.rm = TRUE)    # calculates the mean value of every indiviual row
+print(normalized_each_row)
+median <- median(normalized_each_row)        # calcluates the median of all rows
 
-median_each_row <- apply(normalized_data, 1, median, na.rm = TRUE)     # median for all individual rows
+mean <- mean(normalized_each_row)   # calculates the mean value of all rows
 
-minimum_each_row <- apply(normalized_data, 1, min, na.rm = TRUE) #minimum value for each row
+standard_deviation <- sd(normalized_each_row) #standard deviation for all rows
 
-maximum_each_row <- apply(normalized_data, 1, max, na.rm = TRUE) #maximum value for each row
+min <- min(normalized_each_row) #minimum value of all rows
 
-standarddeviation_each_row <- apply(normalized_data, 1, sd, na.rm = TRUE) #standard deviation for each row
-
-median <- median(median_each_row)        # calcluates the median of all rows
-
-mean <- mean(mean_each_row)   # calculates the mean value of all rows
-
-standard_deviation <- sd(standarddeviation_each_row) #standard deviation for all rows
-
-min <- min(minimum_each_row) #minimum value of all rows
-
-max <- max(maximum_each_row) #maximum value of all rows
+max <- max(normalized_each_row) #maximum value of all rows
 #print results
 cat("Medelvärde av SUS-resultaten:", mean, "\n")
 cat("Median av SUS-resultaten:", median, "\n")
